855e0f932e019bdc37d27fe3b6ffd34d4d928c21
dogovision <donatas.gostautas@itvision.lt>
2021-09-23 10:17:59 +0300

    pushing updates

--- addons/library_member/__manifest__.py
+++ addons/library_member/__manifest__.py
@@ -2,10 +2,13 @@
     'name': 'Library Members',
     'description': 'Manage people who will be able to borrow books.',
     'author': 'Tavo tevas',
-    'depends': ['library_app'],
+    'depends': ['library_app', 'mail'],
     'application': False,
     'data': [
+        'views/book_view.xml',
         'security/library_security.xml',
-        'views/book_view.xml'
+        'security/ir.model.access.csv',
+        'views/member_view.xml',
+        'views/library_menu.xml'
     ]
 }
