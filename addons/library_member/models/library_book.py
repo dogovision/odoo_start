b0df1ab87f03318114b6d49b28613102aba7ec0a
dogovision <donatas.gostautas@itvision.lt>
2021-09-23 11:28:31 +0000

    hoping to push new changes

--- addons/library_member/models/library_book.py
+++ addons/library_member/models/library_book.py
@@ -2,6 +2,15 @@ from odoo import fields, models
 
 class Book(models.Model):
     _inherit = 'library.book'
-    is_available = fields.Boolean('Is Available?')
-    isbn = fields.Char(help="Use a valid ISB-13 or ISBN-10")
-    publisher_id = fields.Many2one(index=True)
+    is_available = fields.Boolean('Is Available?', readonly=False)
+
+    def _check_isbn(self):
+        self.ensure_one()
+        digits = [int(x) for x in self.isbn if x.isdigit()]
+        if len(digits) == 10:
+            ponderators = [x for x in range(1, 10)]
+            total = sum(a * b for a, b in zip(digits[:9], ponderators))
+            check = total % 11
+            return digits[-1] == check
+        else:
+            return super()._check_isbn()
